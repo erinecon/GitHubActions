bbe2d049c10f1fcb80513ec953ce65b7a3726b84
Erin Conley <erin.conley@canonical.com>
2025-06-26 17:01:18 -0400

    write new release notes file

--- release-notes-template.md
+++ release-notes-template.md
@@ -13,26 +13,11 @@ See our [Release policy and schedule](link-to-landing-page).
 For development and testing purposes, a machine or VM with a minimum of <#GB> RAM
 is required. In production, at least <#GB> RAM is recommended per instance.
 
-## What’s new
-
-### Example of a major feature
-
-<Engineers to add more context and information about the entry>
+## What's new
 
 ## Minor features
 
-### Example of a minor feature
-
-* <Engineers to add more context and information about the entry>
-
-### Example of a second minor feature
-
-* <Engineers to add more context and information about the entry>
-
 ## Deprecated features
 
-### Example of a deprecated feature
-
-<Engineers to add more context and information about the entry>
 
 
--- test.py
+++ test.py
@@ -8,11 +8,14 @@ class ChangelogEntry:
         self.linenum = num
         self.items = [] # list of items in the entry
 
-month = "05"
+def substring_after(s, delim):
+    return s.partition(delim)[2]
+
+month = "5"
 year = "2025"
-major_item = "major"
-minor_item = "minor"
-dep_item = "deprecated"
+major_item_tag = "major"
+minor_item_tag = "minor"
+dep_item_tag = "deprecated"
 changelogname = "/Users/econley/GitHubActions/changelog.md"
 
 # first get the line numbers we care about
@@ -47,9 +50,50 @@ for num, entry in enumerate(entries):
     
 f.close()
 
+# sort the items into major, minor, deprecated
+major_items = []
+minor_items = []
+deprecated_items = []
+
+# loop over the entries
+for entry in entries:
+    # check whether the date is appropriate
+    if str(entry.month) == month and str(entry.year) == year:
+        # check all the items and see if they have the right tags
+        for item in entry.items:
+            # items with right tags fill in details of the template
+            # rule: no item can have multiple tags (major, minor, deprecated)
+            # start with major
+            if major_item_tag in item:
+                major_items.append(substring_after(item, "): "))
+            elif minor_item_tag in item:
+                minor_items.append(substring_after(item, "): "))
+            elif dep_item_tag in item:
+                deprecated_items.append(substring_after(item, "): "))
+
+
 # then need to use the template 
 # create a new file
-# loop over the entries
-# check whether the date is appropriate
-# check all the items and see if they have the right tags
-# items with right tags fill in details of the template
+release_notes_file_name = "release-notes-" + str(year) + "-" + str(month) + ".md"
+with open("docs/"+release_notes_file_name, 'w') as outFile:
+    with open("release-notes-template.md") as releaseNotesFile:
+        for line in releaseNotesFile:
+            outFile.write(line)
+            if "What's new" in line.strip():
+                # write all the major items under this section
+                outFile.write("\n")
+                for item in major_items:
+                    outFile.write("### " + item)
+                    outFile.write("\n<Engineers to add more context and information about the entry>\n\n")
+            elif "Minor features" in line.strip():
+                # write all minor items under this section
+                outFile.write("\n")
+                for item in minor_items:
+                    outFile.write("### " + item)
+                    outFile.write("\n<Engineers to add more context and information about the entry>\n\n")
+            elif "Deprecated features" in line.strip():
+                # write all minor items under this section
+                outFile.write("\n")
+                for item in deprecated_items:
+                    outFile.write("### " + item)
+                    outFile.write("\n<Engineers to add more context and information about the entry>\n\n")
